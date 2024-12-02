bce9a80db51811cc1be72f3ea63dfeddfc9dda9f
fasta <fastakovskiy.mihail04@gmail.com>
2024-12-02 13:38:20 +0200

    Добавьте файлы проекта.

---
+++ NetCsharpAnonymMethods/Class.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCsharpAnonymMethods
+{
+    delegate string GetRGBColor(string col);
+    delegate void AddItem(Backpack b,BackpackItem item);
+    delegate int Array1(int[] arr);
+    delegate bool isWord(string word,string text);
+    public class BackpackItem
+    {
+        public string Name { get; set; }   
+        public double Volume { get; set; }
+
+        public BackpackItem(string name, double volume)
+        {
+            Name = name;
+            Volume = volume;
+        }
+    }
+    public class Backpack
+    {
+      public string Color { get; set; }
+        public string Firm {  get; set; }
+        public string ClothType { get; set; }
+        public double Weight { get; set; }
+        public double Volume {  get; set; }
+        public List<BackpackItem> Contents { get; set; }
+        public Backpack()
+        {
+            Color = "";
+            Firm = "";
+            ClothType = "";
+            Weight = 0;
+            Volume = 0;
+            Contents = new List<BackpackItem>();
+        }
+        public Backpack(string col, string f, string ct, double w, double v) {
+            Color = col;
+            Firm = f;
+            ClothType = ct;
+            Weight = w;
+            Volume = v;
+            Contents = new List<BackpackItem>();
+        }
+    
+    }
+}
---
+++ NetCsharpAnonymMethods/Program.cs
@@ -0,0 +1,156 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using NetCsharpAnonymMethods;
+//task1
+//GetRGBColor MyColor = delegate (string col)
+//{
+//    switch (col.ToLower())
+//    {
+//        case "red": return "255, 0, 0";
+//        case "green": return "0, 255, 0";
+//        case "blue": return "0, 0, 255";
+//        case "yellow": return "255, 255, 0";
+//        case "cyan": return "0, 255, 255";
+//        case "magenta": return "255, 0, 255";
+//        default: return "Unknown color";
+//    }
+//};
+
+//Console.WriteLine(MyColor("blue")); 
+//Console.WriteLine(MyColor("red"));    
+
+
+//task2
+//double tempV = 0;
+//AddItem add = delegate (Backpack b, BackpackItem item)
+//{
+//    try
+//    {
+//        if (tempV + item.Volume > b.Volume)
+//        {
+//            throw new Exception("Недостаточно места в рюкзаке!!!!");
+//        }
+
+//    }
+//    catch (Exception ex)
+//    {
+//        Console.WriteLine($"Исключение: {ex.Message}");
+//        return;
+//    }
+
+//    b.Contents.Add(item);
+//    Console.WriteLine("Item was add");
+//    tempV += item.Volume;
+//};
+//Backpack b1=new Backpack("brown", "Ostin&Co", "leather", 1.5, 20);
+//BackpackItem book1 = new BackpackItem("Biology_book", 22);
+
+
+//add(b1, book1);
+
+
+//task3
+Random rnd = new Random();
+//int[] array = new int[10];
+//for (int i = 0; i < array.Length; i++)
+//{
+//    array[i] = rnd.Next(1, 100);
+//    Console.Write(array[i] + " ");
+//}
+//Console.WriteLine();
+
+//Array1 sevenCount = arr =>
+//{
+//    int count = 0;
+//    foreach (int item in arr)
+//    {
+//        if (item % 7 == 0)
+//        {
+//            count++;
+//        }
+//    }
+//    return count;
+//};
+
+//Console.WriteLine("В массиве " + sevenCount(array) + " чисел кратных 7");
+
+
+//task4
+//int[] array2 = new int[10];
+//for (int i = 0; i < array2.Length; i++)
+//{
+//    array2[i] = rnd.Next(-100, 100);
+//    Console.Write(array2[i] + " ");
+//}
+//Console.WriteLine();
+
+//Array1 signCount = arr =>
+//{
+//    int count = 0;
+//    foreach (int item in arr)
+//    {
+//        if (item > 0)
+//        {
+//            count++;
+//        }
+//    }
+//    return count;
+//};
+
+//Console.WriteLine("В массиве " + signCount(array2) + " положительных чисел");
+
+//task5
+
+//int[] array3 = new int[10];
+//for (int i = 0; i < array3.Length; i++)
+//{
+//    array3[i] = rnd.Next(-100, 0);
+//    Console.Write(array3[i] + " ");
+//}
+//Console.WriteLine();
+
+//Array1 unsignCount = arr =>
+//{
+//    int count = 0;
+//    bool isUnique;    
+//   for (int i = 0; i < arr.Length; i++)
+//    {
+//        isUnique = true;
+//        for (int j = 0; j < arr.Length; j++)
+//        {
+//            if (i == j)
+//            {
+//                continue;
+
+//            }
+//            if (arr[i] == arr[j])
+//            {
+//                isUnique = false;
+//                break;
+
+
+//            }
+//        }
+//        if (isUnique == true)
+//        {
+//            count++;
+//        }
+//    }
+//    return count;
+//};
+
+//Console.WriteLine("В массиве " + unsignCount(array3) + " уникальных отрицательных чисел");
+
+
+//task6
+
+isWord word1 = (word, text) =>
+{
+    bool istext = text.ToLower().Contains(word.ToLower());
+    return istext;
+};
+
+if (word1("hello", "helloworld") == true)
+{
+    Console.WriteLine("Yapppiiiiiiii :))");
+}
